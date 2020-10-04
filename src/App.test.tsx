531dc5542e8332dc95308b68dbf60ac6a1df6217
David Armendariz <darmendariz1998@outlook.com>
2020-10-03 20:59:52 -0500

    Used the user event API

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { render, screen, waitFor, fireEvent } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 import { getUser } from './get-user';
 import { mocked } from 'ts-jest/utils';
@@ -67,9 +68,7 @@ describe('When the user enters some text in the input element', () => {
 
     expect(screen.getByText(/You typed: .../));
 
-    fireEvent.change(screen.getByRole('textbox'), {
-      target: { value: 'David' },
-    });
+    await userEvent.type(screen.getByRole('textbox'), 'David');
 
     expect(screen.getByText(/You typed: David/));
   });
